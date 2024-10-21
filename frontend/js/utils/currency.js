dd13dfd58e93b58221413b0e6e834d10c6690fe9
deisgoku <dediiskandar425@gmail.com>
2024-10-21 14:32:53 +0700

    Update currency.js
    rubah ke rupiah
    
    Signed-off-by: deisgoku <dediiskandar425@gmail.com>

--- frontend/js/utils/currency.js
+++ frontend/js/utils/currency.js
@@ -5,6 +5,19 @@
  * @param {*} costInMinimalUnit Cost in minimal unit (cents).
  * @returns Display cost string that may be used in the UI.
  */
+/**
 export function toDisplayCost(costInMinimalUnit) {
     return `\Rp${(costInMinimalUnit / 1.0).toFixed(2)}`;
+}
+*/
+/**
+ * Create display (user-friendly) string cost for the cost in minimal currency unit.
+ * For this app calculations are in IDR (Rupiah).
+ * Example: 1000 => Rp10.000,00
+ * @param {*} costInMinimalUnit Cost in minimal unit (sen).
+ * @returns Display cost string that may be used in the UI.
+ */
+export function toDisplayCost(costInMinimalUnit) {
+    const costInRupiah = costInMinimalUnit / 0.01; // Mengonversi dari sen ke rupiah
+    return `Rp${costInRupiah.toLocaleString('id-ID', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 }
